1de49ff05b9cfb55243fd19e3ea2c37c7f330be0
Heliumwt <35332983+wutong233@users.noreply.github.com>
2022-10-06 18:47:03 +0800

    Update Update.bat
    More appendixes.
    Higher useability.

--- Update.bat
+++ Update.bat
@@ -1,4 +1,5 @@
 ::Written by unomind
+::A poor work learned from baidu.
 @echo off  
 cls
 >nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system" 
@@ -8,23 +9,25 @@ if '%errorlevel%' NEQ '0' (
 ) else ( goto gotAdmin )  
    
 :UACPrompt  
+::Method From CSDN
     echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs" 
     echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs" 
     "%temp%\getadmin.vbs" 
     exit /B  
    
 :gotAdmin  
+::Method From CSDN
     if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )  
     pushd "%CD%" 
     CD /D "%~dp0" 
  
 :begin
-    ping -n 1 www.baidu.com
+    ping -n 2 127.0.0.1
     echo Checking or waiting...
     if exist C:\$WINDOWS.~BT\Sources\appraiserres.dll (goto start) else (goto begin)
 
 :start
-    ping -n 3 www.baidu.com
+    ping -n 2 127.0.0.1
     echo please let me be able to install update! > appraiserres.dll
     copy /y appraiserres.dll C:\$WINDOWS.~BT\Sources
     del appraiserres.dll
